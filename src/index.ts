c877dcfcd9aeb91d7e1785ed0da96260ed28994e
Chris McGravey <chris@mcgravey.dev>
2025-03-25 18:46:22 -0500

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,4 @@
-import { Agent, AgentConfig, ToolCall, WebSearchConfig } from "./Agents/Agent";
-
-import { Tracing, TracingAction, TracingConfig } from "./Tracing/Tracing";
-
-import { Runner, RunnerConfig } from "./Runner/Runner";
-
-import { LoomConfig, ConfigOptions } from "./Loom/Loom";
-
-export {
-  Agent,
-  AgentConfig,
-  ToolCall,
-  WebSearchConfig,
-  Tracing,
-  TracingAction,
-  TracingConfig,
-  Runner,
-  RunnerConfig,
-  LoomConfig,
-  ConfigOptions,
-};
-
-module.exports = {
-  Agent,
-  Tracing,
-  Runner,
-  LoomConfig,
-};
+export * from "./Agents/Agent";
+export * from "./Loom/Loom";
+export * from "./Runner/Runner";
+export * from "./Tracing/Tracing";
